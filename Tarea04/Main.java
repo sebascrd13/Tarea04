4db5a6e6b402c7ad21ad3d258c3ca1c0c2b3aa41
sebascrd13 <106779944+sebascrd13@users.noreply.github.com>
2022-10-09 13:18:16 -0600

    Sede

--- Tarea04/Main.java
+++ Tarea04/Main.java
@@ -1,9 +1,58 @@
 package Tarea04;
+import javax.swing.JOptionPane;
 
 // GIMNASIO
 public class Main {
     public static void main(String[] args) {
+
+        Sede SanPedro = new Sede("Detrás del Mall San Pedro",
+        "De lunes a sábado (8am-10pm)", "Crossfit- Funcional "+
+        "- Kickboxing","Entrenadores, fisioterapeutas y "+
+        "nutricionistas","50 espacios de parqueo"); 
         
+        byte op = 0;
+        while (op != 5) {
+            op = Byte.parseByte(JOptionPane.showInputDialog
+            ("Bienvenido a GIMNASIO"+"\n"+
+            "Digite el número de la sección: "+"\n"+
+            "1. Sede"+"\n"+"2. Cobros"+"\n"+
+            "3. Servicios"+"\n"+ "4. Usuarios" + "\n" + "5. Salir"));
+            
+            if (op == 1) {
+                JOptionPane.showMessageDialog(null,
+                "INFORMACIÓN DE LA SEDE SAN PEDRO"+"\n"+
+                "Ubicación: "+SanPedro.getUbicacion()+"\n"+
+                "Horario: "+ SanPedro.getHorario()+"\n"+
+                "Áreas de Entrenamiento: "+ SanPedro.getAreasEntreno()+"\n"+
+                "Personal: "+ SanPedro.getPersonal()+"\n"+
+                "Parqueo: "+ SanPedro.getParqueo());
+
+                byte opcion = Byte.parseByte(JOptionPane.showInputDialog
+                ("Desea registrarse?" + "\n"+"1 = SI  2 = NO"));
+                
+                if (opcion == 1) {
+                    SanPedro.registro();  
+                }
+            }
+            if (op == 2) {
+                    
+            }
+            if (op == 3) {
+                    
+            }
+            if (op == 4) {
+                    
+            }
+            if (op == 5) {
+                JOptionPane.showMessageDialog(null,
+                "Usted ha salido");
+            }
+            
+            
+        }
+
+
+       
     }
     
 }
--- Tarea04/Sede.java
+++ Tarea04/Sede.java
@@ -1,5 +1,54 @@
 package Tarea04;
+import javax.swing.JOptionPane;
 
 public class Sede {
+    private String ubicacion;
+    private String horario;
+    private String areasEntreno;
+    private String personal;
+    private String parqueo;
+
+    public Sede(String ubicacion, String horario,
+    String areasEntreno, String personal, String parqueo ){
+        this.ubicacion = ubicacion;
+        this.horario = horario;
+        this.areasEntreno = areasEntreno;
+        this.personal = personal;
+        this.parqueo = parqueo;
+
+    }
+
+    public String getUbicacion(){
+        return ubicacion;
+    }
+
+    public String getHorario(){
+        return horario;
+    }
+
+    public String getAreasEntreno(){
+        return areasEntreno;
+    }
+
+    public String getPersonal(){
+        return personal;
+    }
+
+    public String getParqueo(){
+        return parqueo;
+    }
     
+    public void registro(){
+
+        String nombre = JOptionPane.showInputDialog
+        ("Ingrese su nombre: ");
+        String apellido = JOptionPane.showInputDialog
+        ("Ingrese su apellido: ");
+        String hora = JOptionPane.showInputDialog
+        ("Ingrese horario deseado:");
+
+        JOptionPane.showMessageDialog(null,
+        "Nombre: "+ nombre + apellido + "\n" + 
+        "Horario seleccionado: "+ hora );
+    }
 }
