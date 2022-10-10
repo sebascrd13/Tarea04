b8f51571f34d9ec6d172a63cb7a0656ba4938db1
kendal-M <113651167+kendal-M@users.noreply.github.com>
2022-10-09 18:28:18 -0600

    Update Cobros.java

--- Tarea04/Cobros.java
+++ Tarea04/Cobros.java
@@ -7,12 +7,22 @@ public class cobros {
     private String subscripcion;
     private String registro;
 
-    public cobros(String cliente, String subscripcion, String registro)
+    public cobros(String cliente, String subscripcion, String registro){
     this.cliente = cliente;
     this.subscripcion  = subscripcion;
     this.registro  = registro;
 }
 
-public String getCliente(){
+public String getCliente() {
+return cliente;
+}
+
+public String getSubscripcion() {
+return subscripcion;
+}
+
+public String getRegistro(){
+return registro;
+}
 
 }
